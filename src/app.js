b9d0a7b4778d6714185fd6e68a20e8c0ec545417
Victor Katsuo Yamashiro <vkatsuo@gmail.com>
2020-09-06 20:04:29 -0300

    desafio pronto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,88 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body;
+  
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+  
+  const verificaId = repositories.findIndex( repository => repository.id === id );
+
+  if (verificaId < 0) {
+    return response.status(400).json({ erro: "projeto não encontrado"});
+  }
+  
+  const like = repositories[verificaId].likes;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: like,
+  }
+
+  repositories[verificaId] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  
+  const verificaId = repositories.findIndex( repository => repository.id === id );
+
+  if (verificaId < 0) {
+    return response.status(400).json({ erro: "projeto não encontrado"});
+  }
+  
+  repositories.splice(verificaId, 1)
+    
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs, likes } = request.body;
+
+  const verificaId = repositories.findIndex( repository => repository.id === id );
+
+  if (verificaId < 0) {
+    return response.status(400).json({ erro: "projeto não encontrado"});
+  }
+
+  const like = repositories[verificaId].likes + 1;
+  
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: like,
+  }
+
+  repositories[verificaId] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
